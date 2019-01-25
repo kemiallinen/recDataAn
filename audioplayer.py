73b01aa8d23f58bf4ccd1320813227c9c61edc4f
kemiallinen <35116031+kemiallinen@users.noreply.github.com>
2019-01-25 18:24:11 +0100

    finally pygame working on >> a e s t h e t i c s << (only to avoid proper coding)

--- audioplayer.py
+++ audioplayer.py
@@ -2,72 +2,46 @@
 # source: https://github.com/aglla/pyprojects/blob/master/music-player.py
 
 from tkinter import *
-import tkinter.filedialog as tk
-import tkinter.messagebox as tk2
 import pygame
 from scipy.io.wavfile import read
-import sounddevice as sd
 from pygame.sndarray import make_sound
+from PIL import Image, ImageTk
 
-playlist = []
 
+def play():
+    pygame.mixer.music.stop()
+    rate, signal = read('D:\\phd\\DATA\\recordings\\01_ZL\\01_ZL_001.wav')
+    sound = make_sound(signal)
 
-class Application(Frame):
-
-    def __init__(self, master):
-        super(Application, self).__init__(master)
-
-        # self.create_widgets()
-        self.playlistbox = Listbox(self, width=40, height=10,
-                                   selectmode=SINGLE)  # TODO: ---> BROWSE, MULTIPLE, EXTENDED (p.379)
-        for song in playlist:
-            self.playlistbox.insert(END, song)
-
-        self.grid(rowspan=5, columnspan=4)
-        self.playlistbox.grid(row=1)
-        self.playButton = Button(self, text='Play', command=self.play)
-        self.loopButton = Button(self, text='Loop', command=self.loop)
-        self.addButton = Button(self, text='Add', command=self.add)
-        self.playButton.grid(row=4, column=0)
-        self.loopButton.grid(row=4, column=1)
-        self.addButton.grid(row=4, column=2)
-        self.pack()
-
-        # pygame initialize
-        pygame.init()
-        pygame.mixer.pre_init(frequency=44100, channels=1)
-        pygame.mixer.init(frequency=44100, channels=1)
-        print(pygame.mixer.get_init())
-
-    def play(self):
-        # if (len(playlist) == 0):
-        #     tk2.showinfo('Notice', 'No songs in your playlist!\nClick Add to add songs.')
-        # else:
-        pygame.mixer.music.stop()
-        rate, signal = read('D:\\phd\\DATA\\recordings\\01_ZL\\01_ZL_001.wav')
-        sound = make_sound(signal)
-
-        pygame.mixer.music.load(sound)
-        pygame.mixer.music.play(0, 0.0)
-
-    def loop(self):
-        pygame.mixer.music.stop()
-        pygame.mixer.music.play(-1, 0.0)
-
-    def add(self):
-        file = tk.askopenfilenames(initialdir='C:/Users/babbu/Downloads')
-        songsTuple = root.splitlist(file)  # turn user's opened filenames into tuple
-        songsList = list(songsTuple)  # convert to list
-        # Add the full filename of songto playlist list, and a shortened version to the listBox
-        for song in songsList:
-            playlist.append(song);
-            tempArray = song.split('/')
-            songShort = tempArray[len(tempArray) - 1]
-            self.playlistbox.insert(END, songShort)
+    pygame.mixer.Sound(sound).play()
 
 
 root = Tk()
-root.title('Text Editor')
-root.geometry('500x200')
-app = Application(root)
-app.mainloop()
+root.title('19/EXP01')
+root.config(bg="#002d69")
+
+baseh = 100
+
+v = Image.open('.\\img\\uam_full.jpg')
+v = v.resize((int((float(v.size[0])*(baseh/float(v.size[1])))), 100), Image.ANTIALIAS)
+logoU = ImageTk.PhotoImage(v)
+logoUAM = Label(image=logoU)
+
+playButton = Button(root, text='Odtwórz', command=play)
+firstAns = Button(root, text='Ta sama osoba')
+secondAns = Button(root, text='Różne osoby')
+
+modx, mody = 50, 50
+
+logoUAM.grid(row=0, column=0)
+playButton.grid(row=2, column=0, padx=modx, pady=mody)
+firstAns.grid(row=1, column=1, padx=modx, pady=mody)
+secondAns.grid(row=3, column=1, padx=modx, pady=mody)
+        # self.playButton.place(x=0, y=0)
+# root.pack()
+
+pygame.mixer.pre_init(frequency=44100, channels=1)
+pygame.mixer.init()
+
+
+mainloop()
--- pymixtest.py
+++ pymixtest.py
@@ -1,28 +1,20 @@
-# import pygame
+import pygame
 from scipy.io.wavfile import read
-# from pygame.sndarray import make_sound
-import sounddevice as sd
+from pygame.sndarray import make_sound
 import numpy as np
 
-sd.default.samplerate = 44100
-sd.default.device = 4
-# pygame.init()
-# pygame.mixer.pre_init(frequency=44100, channels=1)
-# print(pygame.mixer.get_init())
-# pygame.mixer.music.stop()
-print(sd.query_devices())
-print('\n')
-rate, signal = read('D:\\phd\\DATA\\recordings\\01_ZL\\01_ZL_001.wav')
 
-signal = np.require(signal, requirements='W')
-print(np.max(signal))
-print(len(signal))
-signal = signal/np.max(np.abs(signal))
-# print(np.max(signal))
-print(np.max(signal))
-print(len(signal))
-sd.play(signal, samplerate=rate)
-# sound = make_sound(signal)
-#
-# x = pygame.mixer.Sound(sound)
-# x.play(0, 0.0)
+pygame.mixer.pre_init(frequency=44100, channels=1)
+pygame.mixer.init()
+print(pygame.mixer.get_init())
+
+rate, first_signal = read('D:\\phd\\DATA\\recordings\\01_ZL\\01_ZL_001.wav')
+_, second_signal = read('D:\\phd\\DATA\\recordings\\01_ZL\\01_ZL_002.wav')
+
+signal = np.concatenate((first_signal, second_signal))
+signal += np.random.randint(-2**13, 2**13, size=signal.shape, dtype='int16')
+
+sound_object = make_sound(signal)
+pygame.mixer.stop()
+pygame.mixer.Sound(sound_object).play()
+# pygame.mixer.quit()
