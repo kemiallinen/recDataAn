60671059ee8eaa2403a838f3040a751886a2a71e
kemiallinen <35116031+kemiallinen@users.noreply.github.com>
2019-01-28 19:53:23 +0100

    - added prepare_mix() - added actions for same/different buttons - added log_record (not working properly: pandas df doesn't want to cooperate... YET) - minor change in GUI visual - added manual - added key bindings (press <space> to play, 's' for same speaker, 'd' for different speakers)

--- audioplayer.py
+++ audioplayer.py
@@ -1,47 +1,105 @@
 # Basic Python Music Player - using tkinter and pygame
 # source: https://github.com/aglla/pyprojects/blob/master/music-player.py
+# TODO: app class (maybe it'll fix dataframe and sampling problems)
 
 from tkinter import *
 import pygame
 from scipy.io.wavfile import read
 from pygame.sndarray import make_sound
 from PIL import Image, ImageTk
+import pandas as pd
+import numpy as np
 
 
-def play():
+def play(event=None):
     pygame.mixer.music.stop()
-    rate, signal = read('D:\\phd\\DATA\\recordings\\01_ZL\\01_ZL_001.wav')
-    sound = make_sound(signal)
+    sound = make_sound(mix)
 
     pygame.mixer.Sound(sound).play()
 
 
+def prepare_mix():
+    random_picks = db[['id', 'path']].loc[(db['sex'] == np.random.choice(['f', 'm'], 1)[0]) &
+                                          (db['mod'] == 'n')].sample(n=2)
+    if random_picks.values[0][0] == random_picks.values[1][0]:
+        same_speaker = True
+    else:
+        same_speaker = False
+
+    _, sig1 = read(corename + '\\' + random_picks.values[0][1])
+    _, sig2 = read(corename + '\\' + random_picks.values[1][1])
+
+    mix = np.concatenate((sig1, np.zeros(int(0.5 * rate), dtype='int16'), sig2))
+    mix += np.random.randint(-2 ** 13, 2 ** 13, size=mix.shape, dtype='int16')
+
+    return mix, same_speaker, random_picks
+
+
+def same(event=None):
+    log_record(True == same_speaker)
+
+
+def different(event=None):
+    log_record(False == same_speaker)
+
+
+def log_record(ans):
+    record = {"play_count": 1,
+              "same_speaker": same_speaker,
+              "sig1_name": random_picks.values[0][1],
+              "sig2_name": random_picks.values[1][1],
+              "SNR": 0,
+              "correct": ans,
+              "turning_points": 0}
+    log.append(record, ignore_index=True)
+    print(log)
+
+
+db = pd.read_csv('D:\\phd\\phdDB_test.csv', sep=',', index_col=0)
+corename = 'D:\\phd\\DATA'
+rate = 44100
+log = pd.DataFrame(columns=["play_count", "same_speaker", "sig1_name", "sig2_name",
+                           "SNR", "correct", "turning_points"])
+
+mix, same_speaker, random_picks = prepare_mix()
+
 root = Tk()
 root.title('19/EXP01')
-root.config(bg="#002d69")
+root.config(bg='#002d69')
 
-baseh = 100
+baseh = 150
 
 v = Image.open('.\\img\\uam_full.jpg')
-v = v.resize((int((float(v.size[0])*(baseh/float(v.size[1])))), 100), Image.ANTIALIAS)
+v = v.resize((int((float(v.size[0])*(baseh/float(v.size[1])))), baseh), Image.ANTIALIAS)
 logoU = ImageTk.PhotoImage(v)
 logoUAM = Label(image=logoU)
 
-playButton = Button(root, text='Odtwórz', command=play)
-firstAns = Button(root, text='Ta sama osoba')
-secondAns = Button(root, text='Różne osoby')
+manual = Label(root, text='Kliknij przycisk ⏯, aby odtworzyć nagranie. Usłyszysz dwie następujące po sobie '
+                          'wypowiedzi. \n\n Określ proszę, klikając odpowiedni przycisk, czy wypowiedzi te '
+                          'pochodzą od tego samego, czy od różnych mówców.',
+               fg='#ffffff',
+               bg='#002d69',
+               font=('Calibri', 12),
+               padx=20,
+               pady=20,
+               wraplength=300)
+playButton = Button(root, text='⏯', font=('Calibri', 36), bg='#ffffff', command=play)
+firstAns = Button(root, text='Ta sama osoba', bg='#ffffff', font=('Calibri', 14), width=20, height=5, command=same)
+secondAns = Button(root, text='Różne osoby', bg='#ffffff', font=('Calibri', 14), width=20, height=5, command=different)
 
-modx, mody = 50, 50
-
+modx, mody = 30, 20
 logoUAM.grid(row=0, column=0)
+manual.grid(row=0, column=1)
 playButton.grid(row=2, column=0, padx=modx, pady=mody)
 firstAns.grid(row=1, column=1, padx=modx, pady=mody)
 secondAns.grid(row=3, column=1, padx=modx, pady=mody)
-        # self.playButton.place(x=0, y=0)
-# root.pack()
 
-pygame.mixer.pre_init(frequency=44100, channels=1)
+pygame.mixer.pre_init(frequency=rate, channels=1)
 pygame.mixer.init()
 
+# key bindings
+root.bind('s', same)
+root.bind('d', different)
+root.bind('<space>', play)
 
 mainloop()
--- batch_analysis.py
+++ batch_analysis.py
@@ -3,6 +3,7 @@ from librosa.display import specshow
 import librosa
 import numpy as np
 from pandas import DataFrame
+import pandas as pd
 import csv
 import os
 from scipy.io.wavfile import read
@@ -10,17 +11,22 @@ from matplotlib.pyplot import figure, savefig, axis, close
 from Signal_Analysis.features.signal import get_F_0, get_HNR, get_Jitter
 
 corename = 'D:\\phd\\DATA'
-recordings_core = corename + '\\test_recordings'
+recordings_core = '\\test_recordings'
+speakers_csvname = 'D:\\phd\\SpeakerData.csv'
+
+speakers = pd.read_csv(speakers_csvname, sep=';', index_col=0)
 
 # create dataframe
-df = DataFrame(columns=["initials", "path", "sentence", "mod",
+df = DataFrame(columns=["id", "sex", "path", "sentence", "mod",
                         "F0_mean", "HNR", "jitter", "MFCC", "specgram"])
 
-for speaker_directory in os.listdir(recordings_core):
+for speaker_directory in os.listdir(corename + recordings_core):
 
     # read csv file containing sentences and mod info (n/h/l)
     txtfname = '\\sentences\\' + speaker_directory[:2] + '.csv'
     sentences = []
+
+    speaker_sex = speakers['sex'].loc[speakers['prefix'] == speaker_directory].values[0]
 
     with open(corename + txtfname, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
@@ -28,18 +34,18 @@ for speaker_directory in os.listdir(recordings_core):
             sentences.append(row)
 
     # prepare dirs for mfccs and specgrams
-    mfcc_dir_name = corename + '\\mfcc\\' + speaker_directory
-    if not os.path.isdir(mfcc_dir_name):
-        os.mkdir(mfcc_dir_name)
+    mfcc_dir_name = '\\mfcc\\' + speaker_directory
+    if not os.path.isdir(corename + mfcc_dir_name):
+        os.mkdir(corename + mfcc_dir_name)
 
-    specgram_dir_name = corename + '\\specgrams\\' + speaker_directory
-    if not os.path.isdir(specgram_dir_name):
-        os.mkdir(specgram_dir_name)
+    specgram_dir_name = '\\specgrams\\' + speaker_directory
+    if not os.path.isdir(corename + specgram_dir_name):
+        os.mkdir(corename + specgram_dir_name)
 
-    for wavname in os.listdir(recordings_core + '\\' + speaker_directory):
+    for wavname in os.listdir(corename + recordings_core + '\\' + speaker_directory):
 
         # load wave
-        rate, signal = read(recordings_core + '\\' + speaker_directory + '\\' + wavname)
+        rate, signal = read(corename + recordings_core + '\\' + speaker_directory + '\\' + wavname)
 
         # get f0, Harmonic-to-Noise Ratio (HNR) and jitter values
         recF0mean = get_F_0(signal, rate, min_pitch=70, max_pitch=600)
@@ -54,17 +60,18 @@ for speaker_directory in os.listdir(recordings_core):
         figure()
         specshow(librosa.amplitude_to_db(librosa.magphase(D)[0], ref=np.max))
         axis('off')
-        savefig(specgram_dir_name + '\\' + wavname[-10:-4] + '.png', dpi=200)
+        savefig(corename + specgram_dir_name + '\\' + wavname[-10:-4] + '.png', dpi=200)
         close()
 
         # librosa:: calculate MFCC's (n_mfcc=20) and save *.npy file to relevant dir
         recMFCC = mfcc(signal)
-        np.save(mfcc_dir_name + '\\' + wavname[-10:-4], recMFCC)
+        np.save(corename + mfcc_dir_name + '\\' + wavname[-10:-4], recMFCC)
 
         # export data to pandas dataframe
         count = int(wavname[-7:-4])-1
-        df = df.append({"initials": wavname[-10:-8],
-                        "path": wavname,
+        df = df.append({"id": wavname[:-8],
+                        "sex": speaker_sex,
+                        "path": recordings_core + '\\' + speaker_directory + '\\' + wavname,
                         "sentence": sentences[count][0],
                         "mod": sentences[count][1][0],
                         "F0_mean": recF0mean[0],
