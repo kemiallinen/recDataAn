5d3b5549510e185f6e09c521cd099169509802b1
kemiallinen <35116031+kemiallinen@users.noreply.github.com>
2019-01-29 19:21:26 +0100

    - app converted to class - play button counter - fixed data frame issue (remember about f*ing assignment) - added turning points - after desired num of turning points, app stops and saves data frame to csv file - keys are bound as case insensitive * TODO: SNR adjustment * TODO: normalize all audio files by RMS (future?) * TODO: welcome window with a simple survey and a short experiment info

--- audioplayer.py
+++ audioplayer.py
@@ -1,8 +1,11 @@
 # Basic Python Music Player - using tkinter and pygame
-# source: https://github.com/aglla/pyprojects/blob/master/music-player.py
-# TODO: app class (maybe it'll fix dataframe and sampling problems)
+# based on: https://github.com/aglla/pyprojects/blob/master/music-player.py
+# TODO: SNR adjustment
+# TODO: normalize all audio files by RMS (future?)
+# TODO: welcome window with a simple survey and a short experiment info
 
 from tkinter import *
+from tkinter import messagebox
 import pygame
 from scipy.io.wavfile import read
 from pygame.sndarray import make_sound
@@ -11,95 +14,149 @@ import pandas as pd
 import numpy as np
 
 
-def play(event=None):
-    pygame.mixer.music.stop()
-    sound = make_sound(mix)
+class App(Frame):
 
-    pygame.mixer.Sound(sound).play()
+    def __init__(self, master):
+        super(App, self).__init__(master)
+
+        # TEST SETTINGS, CHANGE BEFORE RUN
+        self.db = pd.read_csv('D:\\phd\\phdDB_test.csv', sep=',', index_col=0)
+        self.stop_after = 2
+        self.verbose = True
+        # END OF TEST SETTINGS
+
+        self.corename = 'D:\\phd\\DATA'
+        self.rate = 44100
+        self.play_count = 0
+        self.turning_points = 0
+        self.log = pd.DataFrame(columns=["play_count", "same_speaker", "sig1_name", "sig2_name",
+                                         "SNR", "correct", "turning_points"])
+
+        self.config(bg='#002d69')
+        self.grid(rowspan=5, columnspan=2)
+
+        baseh = 150
+        v = Image.open('.\\img\\uam_full.jpg')
+        v = v.resize((int((float(v.size[0])*(baseh/float(v.size[1])))), baseh), Image.ANTIALIAS)
+        self.logoU = ImageTk.PhotoImage(v)
+        self.logoUAM = Label(self, image=self.logoU)
+
+        self.manual = Label(self,
+                            text='Kliknij przycisk ⏯, aby odtworzyć nagranie. Usłyszysz dwie następujące po sobie '
+                                  'wypowiedzi. \n\n Określ proszę, klikając odpowiedni przycisk, czy wypowiedzi te '
+                                  'pochodzą od tego samego, czy od różnych mówców.',
+                            fg='#ffffff',
+                            bg='#002d69',
+                            font=('Calibri', 12),
+                            padx=20, pady=20,
+                            wraplength=300)
+
+        self.playButton = Button(self,
+                                 text='⏯',
+                                 font=('Calibri', 36),
+                                 bg='#ffffff',
+                                 command=self.play)
+
+        self.firstAns = Button(self,
+                               text='Ta sama osoba [s]',
+                               bg='#ffffff',
+                               font=('Calibri', 14),
+                               width=20, height=5,
+                               command=self.same)
+
+        self.secondAns = Button(self,
+                                text='Różne osoby [d]',
+                                bg='#ffffff',
+                                font=('Calibri', 14),
+                                width=20, height=5,
+                                command=self.different)
+
+        modx, mody = 30, 20
+        self.logoUAM.grid(row=0, column=0)
+        self.manual.grid(row=0, column=1)
+        self.playButton.grid(row=2, column=0, padx=modx, pady=mody)
+        self.firstAns.grid(row=1, column=1, padx=modx, pady=mody)
+        self.secondAns.grid(row=3, column=1, padx=modx, pady=mody)
+
+        pygame.mixer.pre_init(frequency=self.rate, channels=1)
+        pygame.mixer.init()
+
+        self.mix, self.same_speaker, self.random_picks = None, None, None
+        self.prepare_mix()
+
+    def play(self, event=None):
+        self.play_count += 1
+        pygame.mixer.music.stop()
+        sound = make_sound(self.mix)
+
+        pygame.mixer.Sound(sound).play()
+
+    def prepare_mix(self):
+        self.random_picks = self.db[['id', 'path']].loc[(self.db['sex'] == np.random.choice(['f', 'm'], 1)[0]) &
+                                                        (self.db['mod'] == 'n')].sample(n=2)
+        if self.random_picks.values[0][0] == self.random_picks.values[1][0]:
+            self.same_speaker = True
+        else:
+            self.same_speaker = False
+
+        _, sig1 = read(self.corename + '\\' + self.random_picks.values[0][1])
+        _, sig2 = read(self.corename + '\\' + self.random_picks.values[1][1])
+
+        self.mix = np.concatenate((sig1, np.zeros(int(0.5 * self.rate), dtype='int16'), sig2))
+        self.mix += np.random.randint(-2 ** 10, 2 ** 10, size=self.mix.shape, dtype='int16')
+
+    def same(self, event=None):
+        self.log_record(self.same_speaker == True)
+
+    def different(self, event=None):
+        self.log_record(self.same_speaker == False)
+
+    def log_record(self, ans):
+
+        if not self.log.empty:
+            if self.log['correct'].values[-1] != ans:
+                self.turning_points += 1
+
+        self.log = self.log.append({"play_count": self.play_count,
+                                    "same_speaker": self.same_speaker,
+                                    "sig1_name": self.random_picks.values[0][1].split(sep='\\')[-1],
+                                    "sig2_name": self.random_picks.values[1][1].split(sep='\\')[-1],
+                                    "SNR": 0,
+                                    "correct": ans,
+                                    "turning_points": self.turning_points},
+                                   ignore_index=True)
+        if self.verbose:
+            print(self.log[['correct', 'turning_points']])
+
+        if self.turning_points == self.stop_after:
+            self.stop_and_save()
+
+        self.play_count = 0
+        self.prepare_mix()
+
+    def stop_and_save(self):
+        self.log.to_csv('results.csv', index=True, header=True, encoding='utf-8')
+        messagebox.showinfo('Koniec eksperymentu',
+                            'Koniec eksperymentu 19/EXP01\n'
+                            'Serdecznie dziękuję!')
+        global root
+        root.destroy()
 
 
-def prepare_mix():
-    random_picks = db[['id', 'path']].loc[(db['sex'] == np.random.choice(['f', 'm'], 1)[0]) &
-                                          (db['mod'] == 'n')].sample(n=2)
-    if random_picks.values[0][0] == random_picks.values[1][0]:
-        same_speaker = True
-    else:
-        same_speaker = False
+def bind_keys():
+    global root
+    root.bind('s', apka.same)
+    root.bind('S', apka.same)
+    root.bind('d', apka.different)
+    root.bind('D', apka.different)
+    root.bind('<space>', apka.play)
 
-    _, sig1 = read(corename + '\\' + random_picks.values[0][1])
-    _, sig2 = read(corename + '\\' + random_picks.values[1][1])
-
-    mix = np.concatenate((sig1, np.zeros(int(0.5 * rate), dtype='int16'), sig2))
-    mix += np.random.randint(-2 ** 13, 2 ** 13, size=mix.shape, dtype='int16')
-
-    return mix, same_speaker, random_picks
-
-
-def same(event=None):
-    log_record(True == same_speaker)
-
-
-def different(event=None):
-    log_record(False == same_speaker)
-
-
-def log_record(ans):
-    record = {"play_count": 1,
-              "same_speaker": same_speaker,
-              "sig1_name": random_picks.values[0][1],
-              "sig2_name": random_picks.values[1][1],
-              "SNR": 0,
-              "correct": ans,
-              "turning_points": 0}
-    log.append(record, ignore_index=True)
-    print(log)
-
-
-db = pd.read_csv('D:\\phd\\phdDB_test.csv', sep=',', index_col=0)
-corename = 'D:\\phd\\DATA'
-rate = 44100
-log = pd.DataFrame(columns=["play_count", "same_speaker", "sig1_name", "sig2_name",
-                           "SNR", "correct", "turning_points"])
-
-mix, same_speaker, random_picks = prepare_mix()
 
 root = Tk()
 root.title('19/EXP01')
-root.config(bg='#002d69')
 
-baseh = 150
+apka = App(root)
 
-v = Image.open('.\\img\\uam_full.jpg')
-v = v.resize((int((float(v.size[0])*(baseh/float(v.size[1])))), baseh), Image.ANTIALIAS)
-logoU = ImageTk.PhotoImage(v)
-logoUAM = Label(image=logoU)
+bind_keys()
 
-manual = Label(root, text='Kliknij przycisk ⏯, aby odtworzyć nagranie. Usłyszysz dwie następujące po sobie '
-                          'wypowiedzi. \n\n Określ proszę, klikając odpowiedni przycisk, czy wypowiedzi te '
-                          'pochodzą od tego samego, czy od różnych mówców.',
-               fg='#ffffff',
-               bg='#002d69',
-               font=('Calibri', 12),
-               padx=20,
-               pady=20,
-               wraplength=300)
-playButton = Button(root, text='⏯', font=('Calibri', 36), bg='#ffffff', command=play)
-firstAns = Button(root, text='Ta sama osoba', bg='#ffffff', font=('Calibri', 14), width=20, height=5, command=same)
-secondAns = Button(root, text='Różne osoby', bg='#ffffff', font=('Calibri', 14), width=20, height=5, command=different)
-
-modx, mody = 30, 20
-logoUAM.grid(row=0, column=0)
-manual.grid(row=0, column=1)
-playButton.grid(row=2, column=0, padx=modx, pady=mody)
-firstAns.grid(row=1, column=1, padx=modx, pady=mody)
-secondAns.grid(row=3, column=1, padx=modx, pady=mody)
-
-pygame.mixer.pre_init(frequency=rate, channels=1)
-pygame.mixer.init()
-
-# key bindings
-root.bind('s', same)
-root.bind('d', different)
-root.bind('<space>', play)
-
-mainloop()
+apka.mainloop()
